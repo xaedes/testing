4d4128b0314a278706d99aee4b29b2e7f57bc399
Martin Hünermund <xaedes@googlemail.com>
2015-12-19 20:22:34 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['testing'],
+    package_dir={'': 'src'}
+    ##  don't do this unless you want a globally visible script
+    # scripts=['bin/myscript'],
+    # requires=[]
+)
+
+setup(**d)
